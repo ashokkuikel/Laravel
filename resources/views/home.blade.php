553d1202a0bd28b965ed07a6eb6e0f9f5c6cf03f
dreamspark71 <dreamspark71@gmail.com>
2018-08-18 23:11:32 +0545

    Navbar fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,25 @@
-@extends('layouts.app')
+@extends('layouts.admin')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+<div class="container" id="dash-container">
+    <div class="row">
+        <div class="col-md-3">
             <div class="card">
                 <div class="card-header">Dashboard</div>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
 
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+        <div class="col-md-9">
+             <div class="card">
+                <div class="card-header">Dashboard</div>
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success">
